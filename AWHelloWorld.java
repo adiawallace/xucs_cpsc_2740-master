4bb1aca8b33ff8f5beed67f366c33a317478764d
adiawallace <a.mikhael.wallace@gmail.com>
2014-03-16 12:51:54 -0500

    *crossing fingers again*
    Pushed to the wrong repository last time

---
+++ AWHelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class AdiaWallace {
+    
+    public static void main( String[] args ) {
+	System.out.println("Hello, World!");
+
+    }
+}
---
+++ DegreeAudit/src/degreeaudit/DegreeAudit.java
@@ -0,0 +1,36 @@
+package degreeaudit;
+
+/**
+ * CPSC Degree Audit application.
+ *
+ * @author CPSC 2740, Spring 2014
+ */
+public class DegreeAudit {
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        tnguye91();
+        mtran14();
+        nsoifer();
+    }
+
+    public static void displayWelcomeMessage() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void tnguye91() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void mtran14() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void nsoifer() {
+
+    }
+
+    public static void cBellIsGreat() {
+        System.out.println("It's true.");
+    }
+}
